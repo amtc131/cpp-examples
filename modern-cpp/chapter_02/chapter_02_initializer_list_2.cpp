e16720a6a6fec0b8a1c5bb5c2acc4626b617b3a4
amtc131 <amtc131@gmail.com>
2023-03-20 16:40:45 -0600

    add variables and initialization

---
+++ modern-cpp/chapter_02/chapter_02_initializer_list_2.cpp
@@ -0,0 +1,29 @@
+#include <initializer_list>
+#include <vector>
+#include <iostream>
+
+class MagicFoo{
+  public:
+    std::vector<int> vec;
+    //MagicFoo(std::initializer_list<int> list) {
+    //  for (std::initializer_list<int>::iterator it = list.begin(); it != list.end(); ++it)
+    //    vec.push_back(*it);
+   // }
+    void foo(std::initializer_list<int> list){
+      for (std::initializer_list<int>::iterator it = list.begin();
+          it != list.end(); ++it) vec.push_back(*it);
+    }
+
+};
+
+int main(){
+  //after c++11
+  // MagicFoo magicFoo = {1,2,3,4,5}; 
+  MagicFoo magicFoo;
+  magicFoo.foo({1,2,3,4,5});
+
+  std::cout << "magicFoo: " ;
+  for(std::vector<int>::iterator it = magicFoo.vec.begin();
+      it != magicFoo.vec.end(); ++it)
+    std::cout << *it << std::endl;
+}
